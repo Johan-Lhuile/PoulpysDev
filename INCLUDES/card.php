f0b56d0a9ddfc09e6c2cda0a42c28421b034c1e2
Johan Lhuile <111600247+Johan-Lhuile@users.noreply.github.com>
2023-07-14 09:06:13 +0200

    Update card.php

--- INCLUDES/card.php
+++ INCLUDES/card.php
@@ -1,7 +1,7 @@
 <div id="card">
 
   <div class="card">
-    <div class="card_img"><img src="" alt=""></div>
+    <div class="card_img"><img src="#" alt=""></div>
     <div class="card_text">
       <h2>Prenom NOM</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius alias temporibus labore repellendus, tempora
@@ -14,9 +14,9 @@
   </div>
 
   <div class="card_bottom"><a href="github"></a>
-    <a href=""><img src="" alt=""></a>
-    <a href=""><img src="" alt=""></a>
-    <a href=""><img src="" alt=""></a>
+    <a href=""><img src="#" alt=""></a>
+    <a href=""><img src="#" alt=""></a>
+    <a href=""><img src="#" alt=""></a>
   </div>
 
 </div>
