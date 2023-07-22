40915f5a359a70fab0125d78d464b2b40b5cd18b
Moony89 <imene.hamzaoui@gmail.com>
2023-07-22 11:06:31 +0200

    EncoreUnPeuDeCSS

--- INCLUDES/footer.php
+++ INCLUDES/footer.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="../CSS/footer.css">
 </head>
+
 <body>
-  
+
 </body>
+
 </html>
 
 <footer>
-  <p>  Poulpy's Dev Project &#169 Tous droits réservés</p>
+  <button><span> &#8679; </span></button>
+  <p>
+    Poulpy's Dev Project &#169 Tous droits réservés
+  </p>
 </footer>
